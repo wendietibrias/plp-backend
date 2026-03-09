729ff740f2cd4593f305e71d051c6d48cba991b6
CodeNoobies <dev.justcallmesu@gmail.com>
2026-03-09 11:30:22 +0700

    fix(cors): add 'http://127.0.0.1:5175' to allowed origins

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -8,6 +8,7 @@ export const CorsConfig = registerAs(
       'http://localhost:3000',
       'http://localhost:5173',
       'http://localhost:5175',
+      'http://127.0.0.1:5175',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
