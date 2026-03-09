93cf4e47100fe9ac6d1f4cbb671be7c6f972f6a5
CodeNoobies <dev.justcallmesu@gmail.com>
2026-03-09 14:16:41 +0700

    fix(main): change ValidationPipe to allow non-whitelisted properties

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
+      forbidNonWhitelisted: false,
       transform: true,
     }),
   );
