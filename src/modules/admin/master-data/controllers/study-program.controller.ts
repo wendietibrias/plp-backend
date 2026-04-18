174cd790ad04384e757766dc1156b7ee5684a3f8
wendietibrias <wendietibrias@gmail.com>
2026-04-18 19:34:56 +0700

    feat (master-data): study program (wip)

--- src/modules/admin/master-data/controllers/study-program.controller.ts
+++ src/modules/admin/master-data/controllers/study-program.controller.ts
@@ -1,28 +1,68 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
 import { UseAuth } from "../../../../commons/decorators/auth.decorator";
 import { StudyProgramService } from "../services/study-program.service";
 import { ApiBearerAuth, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { CreateStudyProgramDto, StudyProgramQuery, UpdateStudyProgramDto } from "../dtos/study-program.dto";
 
 @ApiTags("Study Program - Endpoint")
 @ApiBearerAuth('JWT')
 @UseAuth()
-@Controller({ path:'study-program', version:'1' })
+@Controller({ path: 'study-program', version: '1' })
 export class StudyProgramController {
     constructor(
         private readonly studyProgramService: StudyProgramService
-    ){}
+    ) { }
 
 
     @Get()
     @ApiOkResponse({
-         description:"Endpoint untuk manipulasi data program studi",
-         schema: {
+        description: "Endpoint untuk manipulasi data program studi",
+        schema: {
             example: {
-                 status: 'success',
-                 items: [],
-                 meta: {}
+                status: 'success',
+                items: [],
+                meta: {}
             }
-         }
+        }
     })
-    async findAll(@Query()){}
+    async findAll(@Query() studyProgramQuery: StudyProgramQuery) {
+        return await this.studyProgramService.findAll(studyProgramQuery);
+
+    }
+
+    @ApiOkResponse({
+        description: "Endpoint untuk manipulasi data program studi",
+        schema: {
+            example: {}
+        }
+    })
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return await this.studyProgramService.findOne(id);
+    }
+
+    @Post()
+    @ApiOkResponse({
+        description: "Endpoint untuk manipulasi data program studi",
+        schema: {
+            example: {
+                message:""
+            }
+        }
+    })
+    async create(@Body() createStudyProgramDto: CreateStudyProgramDto) {
+        return await this.studyProgramService.create(createStudyProgramDto);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateStudyProgramDto: UpdateStudyProgramDto){
+        return await this.studyProgramService.update(id,updateStudyProgramDto);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string){
+        return await this.studyProgramService.delete(id);
+    }
+
 }
--- src/modules/admin/master-data/services/study-program.service.ts
+++ src/modules/admin/master-data/services/study-program.service.ts
@@ -1,15 +1,94 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException, Param, ParseIntPipe } from "@nestjs/common";
+import { CreateStudyProgramDto, StudyProgramQuery, UpdateStudyProgramDto } from "../dtos/study-program.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { StudyProgram } from "../study-program.entity";
+import { Repository } from "typeorm";
+import { customPaginate } from "../../../../commons/utils/paginate";
+import {  like } from "../../../../commons/utils/orm";
 
 @Injectable()
 export class StudyProgramService {
+    constructor(
+        @InjectRepository(StudyProgram) private readonly studyProgramRepository: Repository<StudyProgram>
+    ){}
     
-    async findAll(){}
+    async findAll(studyProgramQuery: StudyProgramQuery){
+        try {
+          const { page,limit,search } = studyProgramQuery;
+          const queryBuilder = this.studyProgramRepository.createQueryBuilder('studyProgram');
 
-    async findOne(){}
+          if(search){
+            queryBuilder.andWhere(like('studyProgram.name',search));
+          }
 
-    async create(){}
+          return await customPaginate(queryBuilder, { page,limit })
+        } catch(err){
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+    }
 
-    async update(){}
+    async findOne(id: string){
+        try{
+          const findDetail = await this.studyProgramRepository.findOne({
+             where:{
+                id:id,
+             }
+          });
 
-    async delete(){}
+          if(!findDetail){
+            throw new NotFoundException("Program studi  tidak ditemukan");
+          }
+          return findDetail;
+        } catch(err) {
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+    }
+
+    async create(createStudyProgramDto: CreateStudyProgramDto){
+        try {
+           const createStudyProgram = this.studyProgramRepository.create(createStudyProgramDto);
+           const saveStudyProgram = await this.studyProgramRepository.save(createStudyProgram);
+           if(saveStudyProgram){
+            return{
+                message:"Berhasil menambahkan program studi"
+            }
+           }
+
+        } catch(err) {
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+    }
+
+    async update(id: string,updateStudyProgramDto: UpdateStudyProgramDto){
+          try {
+           const findDetail = await this.findOne(id);
+           const createStudyProgram = this.studyProgramRepository.create({
+            ...findDetail,
+            ...updateStudyProgramDto
+           });
+
+           const saveStudyProgram = await this.studyProgramRepository.save(createStudyProgram);
+           if(saveStudyProgram){
+            return{
+                message:"Berhasil mengedit program studi"
+            }
+           }
+
+        } catch(err) {
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+    }
+
+    async delete(id: string){
+        try {
+          const deleteStudyProgram = await this.studyProgramRepository.delete(id);
+          if(deleteStudyProgram){
+            return {
+                message:"Berhasil menghapus program studi"
+            }
+          }
+        } catch(err) {
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+    }
 }
